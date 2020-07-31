2d60ad8efccfe41eefcff1b9224dc500eec80716
James <jtlopez919@gmail.com>
2020-07-30 20:55:37 -0400

    init

---
+++ src/component/pages/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home-page">
+            <div className="title">
+                
+            </div>
+            <div className="content">
+                <div className="latest-works-title">
+                    <p id="chapter">Projects</p>
+                    <p id="chapter-content">Latest Works</p>
+                </div>
+                <div className="latest-works-content">
+                    <div className="card">
+
+                    </div>
+                    <div className="card">
+
+                    </div>
+                    <div className="card">
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
