7b1cdf975d7427a053e33fc20c6eea57bf7460bc
James <jtlopez919@gmail.com>
2020-08-02 21:52:01 -0400

    added pictures to projects and techincal skills
    -improved css and animations

--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import Chordify from '../../images/chordify.png'
+import Frontend from '../../images/frontendLarge.png'
+import Lyricly from '../../images/lyricly.png'
+import OnlineWhite from '../../images/online.png'
+import Server from '../../images/server.png'
 
 const Home = () => {
     return (
         <div className="home-page">
             <section className="title">
+                <header></header>
+                
             </section>
                 <section className="content">
                     <div className="project-content">
@@ -15,27 +21,49 @@ const Home = () => {
                         </div>
                         <div className="latest-works-content">
                             <div className="card-container">
-                                <div className="card">
+                                <div className="card hov1">
                                     <div className="image-box">
                                         <img id="chordify" src={Chordify} alt="chordify" />
                                     </div>
                                     <div className="content-box">
                                         <p>Chordify</p>
                                         <div className="button-group">
-                                            <section><button className="but">Demo</button></section>
-                                            <section><button className="but">About</button></section>
-                                            <section><button className="but">Source Code</button></section>
+                                            <section><button className="but1">Demo</button></section>
+                                            <section><button className="but2">About</button></section>
+                                            <section><button className="but3">Source Code</button></section>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <div className="card-container">
-                                <div className="card">
+                                <div className="card hov2">
+                                <div className="image-box">
+                                        <img id="lyricly" src={Lyricly} alt="lyricly" />
+                                    </div>
+                                    <div className="content-box">
+                                        <p>Lyricly</p>
+                                        <div className="button-group">
+                                            <section><button className="but1">Demo</button></section>
+                                            <section><button className="but2">About</button></section>
+                                            <section><button className="but3">Source Code</button></section>
+                                        </div>
+                                    </div>
 
                                 </div>
                             </div>
                             <div className="card-container">
-                                <div className="card">
+                                <div className="card hov3">
+                                <div className="image-box">
+                                        <img id="onlinewhite" src={OnlineWhite} alt="online whiteboard" />
+                                    </div>
+                                    <div className="content-box">
+                                        <p>Whiteboard</p>
+                                        <div className="button-group">
+                                            <section><button className="but1">Demo</button></section>
+                                            <section><button className="but2">About</button></section>
+                                            <section><button className="but3">Source Code</button></section>
+                                        </div>
+                                    </div>
 
                                 </div>
                             </div>
@@ -50,22 +78,36 @@ const Home = () => {
                             </div>
                             <div className="toolkit-works">
                                 <div className="toolkit-container">
-                                    <section>
-                                        <p>Languages</p>
-                                        <ul>
-                                            <li>Javascript</li>
-                                            <li>Typescript</li>
-                                            <li>HTML5</li>
-                                            <li>CSS or SASS</li>
-                                        </ul>
+                                    <section >
+                                        <div className="image-box">
+                                            <img id="toolkit-images" src={Frontend} alt="Frontend"/>
+                                        </div>
+                                        <div className="content-box">
+                                            <p>Frontend</p>
+                                            <div id="list">
+                                                Javascript<br/>
+                                                Typescript<br/>
+                                                HTML5<br/>
+                                                CSS or SASS<br/>
+                                            </div>
+                                        </div>
                                     </section>
+
+       
                                     <section>
-                                        <p>Libraries or Frameworks</p>
-                                        <ul>
-                                            <li>React</li>
-                                            <li>Bootstrap</li>
-                                            <li>Node</li>
-                                        </ul>
+                                        <div className="image-box">
+                                            <img id="toolkit-images" src={Server} alt="Frontend"/>
+                                        </div>
+                                        <div className="content-box">
+                                            <p>Libraries/Frameworks</p>
+                                            <div id="list">
+                                                React<br/>
+                                                Bootstrap<br/>
+                                                Node<br/>
+                                                <br/>
+                                            </div>
+                                        </div>
+                                      
                                     </section>
                                     <section>
                                         <p> Web Design tools</p>
