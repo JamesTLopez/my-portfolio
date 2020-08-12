306b13b72112d013cf72d65136c8a85915a9ef97
James <jtlopez919@gmail.com>
2020-08-12 13:54:44 -0400

    improved portfolio card ui
    -improved UI and added header
    -

--- src/component/layout/ToolkitCard.js
+++ src/component/layout/ToolkitCard.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import '../../styles/toolkitcard.css'
 
-function ToolkitCard() {
-    return (
-        <div className="card-container">
-            
+function ToolkitCard({ name, image, id, hov }) {
+  return (
+    <div className="card-container">
+      <div className={`card ${hov}`}>
+        <div className="image-box">
+          <img id={id} src={image} alt={id}></img>
         </div>
-    )
+        <div className="content-box">
+
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ToolkitCard
+export default ToolkitCard;
--- src/component/pages/Home.js
+++ src/component/pages/Home.js
@@ -1,5 +1,10 @@
 import React from "react";
+import {Link} from "react-router-dom"
+//Components
 import ProjectCard from "../layout/ProjectCard";
+import ToolkitCard from "../layout/ToolkitCard";
+
+//Images
 import Chordify from "../../images/chordify.png";
 import Lyricly from "../../images/lyricly.png";
 import OnlineWhite from "../../images/online.png";
@@ -11,10 +16,24 @@ const Home = () => {
   return (
     <div className="home-page">
       <section className="title">
-        <header></header>
+        <header>
+          <div className="Name">
+            <p>James Lopez</p>
+          </div>
+          <div className="navigation">
+            <nav>
+              <ul>
+                <li><Link id="Link" path="/">Home</Link></li>
+                <li><Link id="Link" path="/about">About</Link></li>
+                <li><Link id="Link" path="/contact">Contact</Link></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
       </section>
       <section className="content">
         <div className="project-content">
+          <div className="project-header"></div>
           <div className="latest-works-title">
             <p id="chapter">Projects</p>
             <p id="chapter-content">Latest Works</p>
@@ -47,7 +66,24 @@ const Home = () => {
               <p id="chapter-content">Technologies</p>
             </div>
             <div class="toolkit-cards">
-
+              <ToolkitCard
+                name="Frontend"
+                image={Frontend}
+                id="frontend"
+                hov="hov1"
+              />
+              <ToolkitCard
+                name="Server"
+                image={Server}
+                id="server"
+                hov="hov2"
+              />
+              <ToolkitCard
+                name="Webtech"
+                image={Webtech}
+                id="webtech"
+                hov="hov3"
+              />
             </div>
           </div>
         </div>
