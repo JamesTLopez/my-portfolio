5a59af6478b38462c5bd1bbe5bbe6a82fe1ce8d8
James <jtlopez919@gmail.com>
2020-08-31 18:07:55 -0400

    Updates

---
+++ precache-manifest.b3f6779d435192a05bf221024e3ceef8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "531e99f78d28798d91b66e0228b4ba96",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "572c7b2dfb924e5582d1",
+    "url": "/my-portfolio/static/css/main.9b404698.chunk.css"
+  },
+  {
+    "revision": "4fb0f33c11abc9a22f0f",
+    "url": "/my-portfolio/static/js/2.dd3c581e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.dd3c581e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572c7b2dfb924e5582d1",
+    "url": "/my-portfolio/static/js/main.74390b39.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "9a4c12cd02634eebb4d1a3cdbe28c996",
+    "url": "/my-portfolio/static/media/Gitcat.9a4c12cd.png"
+  },
+  {
+    "revision": "811ed1bda553dec20b6293e2ee882e71",
+    "url": "/my-portfolio/static/media/affinitydesigner.811ed1bd.png"
+  },
+  {
+    "revision": "a86ff765090752bd3fbfbc769d5b2316",
+    "url": "/my-portfolio/static/media/affinityphoto.a86ff765.png"
+  },
+  {
+    "revision": "510d225d08cbe022f7e2cbd83c69eee4",
+    "url": "/my-portfolio/static/media/copy.510d225d.png"
+  },
+  {
+    "revision": "63f6d16af5ff3ae633609fad00b07dba",
+    "url": "/my-portfolio/static/media/css.63f6d16a.png"
+  },
+  {
+    "revision": "25a2f0fe678bc52332299a522ddb768b",
+    "url": "/my-portfolio/static/media/email.25a2f0fe.png"
+  },
+  {
+    "revision": "f176180d4bfd985e623fe4f10430b5b7",
+    "url": "/my-portfolio/static/media/frontendLarge.f176180d.png"
+  },
+  {
+    "revision": "7f3976dce33c0e23e9c0208eafb08420",
+    "url": "/my-portfolio/static/media/git.7f3976dc.png"
+  },
+  {
+    "revision": "1b1c7083204ba71f9e8e44776fcdd70f",
+    "url": "/my-portfolio/static/media/html.1b1c7083.png"
+  },
+  {
+    "revision": "50a67f1cbe0f46b4cd999a8d083155bc",
+    "url": "/my-portfolio/static/media/javascript.50a67f1c.png"
+  },
+  {
+    "revision": "481102a329f5d4493c10278f4e177a9c",
+    "url": "/my-portfolio/static/media/lyricly.481102a3.png"
+  },
+  {
+    "revision": "c6502c5ae386f24fb8e0e40af79e2cb3",
+    "url": "/my-portfolio/static/media/node.c6502c5a.png"
+  },
+  {
+    "revision": "c7b5ccd2fa745115a95b96641b23984c",
+    "url": "/my-portfolio/static/media/online.c7b5ccd2.png"
+  },
+  {
+    "revision": "3d05f42ce0af478498ba8f48736dc5d2",
+    "url": "/my-portfolio/static/media/react.3d05f42c.png"
+  },
+  {
+    "revision": "81c0a2b18e539f72e735a7d846be2952",
+    "url": "/my-portfolio/static/media/resume.81c0a2b1.png"
+  },
+  {
+    "revision": "3a39ac11039e4fbcd0016b00337a2752",
+    "url": "/my-portfolio/static/media/sass.3a39ac11.png"
+  },
+  {
+    "revision": "74d58826db05a5ce27bd26618d63663d",
+    "url": "/my-portfolio/static/media/titl.74d58826.jpg"
+  },
+  {
+    "revision": "ad1513c349bc337ee3e0b0d289362213",
+    "url": "/my-portfolio/static/media/type.ad1513c3.png"
+  }
+]);
